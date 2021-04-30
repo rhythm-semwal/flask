5bfc893a00a0f893d813b267dc06a637e75061e3
rhythm semwal <08rhythm@gmail.com>
2021-04-30 15:30:05 +0530

    updated linkedlist.py file

--- DataStructureWithFlask/linkedlist.py
+++ DataStructureWithFlask/linkedlist.py
@@ -1,5 +1,35 @@
 class Node:
     def __init__(self, data=None, next_node=None):
         self.data = data
-        self.next_node = next_node
+        self.next = next_node
 
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+
+    def print_linked_list(self):
+        if self.head is None:
+            print("None")
+
+        node = self.head
+        ll_string = ""
+
+        while node:
+            ll_string += f"{str(node.data)} ->"
+            node = node.next
+
+        ll_string += "None"
+
+        print(ll_string)
+
+
+ll = LinkedList()
+node4 = Node("data4", None)
+node3 = Node("data3", node4)
+node2 = Node("data2", node3)
+node1 = Node("data1", node2)
+
+ll.head = node1
+ll.print_linked_list()
