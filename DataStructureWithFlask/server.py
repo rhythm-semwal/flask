22cb8fd030775ae6febd9d60a28552c6bccc60a1
rhythm semwal <08rhythm@gmail.com>
2021-04-30 13:14:44 +0530

    First Commit

---
+++ DataStructureWithFlask/server.py
@@ -0,0 +1,108 @@
+from sqlite3 import Connection as SQLite3Connection
+from datetime import datetime
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# app
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+
+
+@event.listens_for(Engine, "connect")
+# configure sqlite3 to enforce foreign key constraints
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+
+class User(db.Model):
+    # models: creating DDL for User table
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost", cascade="all, delete")
+
+
+class BlogPost(db.Model):
+    # models: creating DDL for BlogPost table
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+# creating routes
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data['email'],
+        address=data["address"],
+        phone = data["phone"]
+    )
+
+    # inserting new user in the db
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "user Created"}), 200
+
+
+@app.route('/user/descending_id', methods=['GET'])
+def get_user_descending():
+    pass
+
+
+@app.route('/user/ascending_id', methods=['GET'])
+def get_user_ascending():
+    pass
+
+
+@app.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    pass
+
+
+@app.route('/user/ascending_id', methods=['DELETE'])
+def delete_user(user_id):
+    pass
+
+
+@app.route('/user/<user_id>', methods=['POST'])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route('/user/<user_id>', methods=['GET'])
+def get_all_blog_post(user_id):
+    pass
+
+
+@app.route('/blog_post/<blog_post_id>', methods=['GET'])
+def get_one_blog_post(blog_post_id):
+    pass
+
+
+@app.route('/blog_post/<blog_post_id>', methods=['DELETE'])
+def delete_blog_post(blog_post_id):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
